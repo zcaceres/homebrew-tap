a5bdc02179c6c6b482e7f62f13c720aa06828de0
Zach Caceres <zcaceres@users.noreply.github.com>
2026-03-18 18:16:46 -0600

    Update builtwith formula to v3.2.0

--- Formula/builtwith.rb
+++ Formula/builtwith.rb
@@ -2,25 +2,25 @@ class Builtwith < Formula
   desc "Query the BuiltWith API from your terminal"
   homepage "https://builtwith.zach.dev"
   license "MIT"
-  version "3.1.0"
+  version "3.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/zcaceres/builtwith-api/releases/download/v#{version}/builtwith-darwin-arm64"
-      sha256 "c210d1ecc348a08ec045a26302df6c99e14f2818bbd253b403fe34a6092674d0"
+      sha256 "14897b5286388adcfed00ce13cd533b86714b3001cf30d8832d5fa79ae476258"
     else
       url "https://github.com/zcaceres/builtwith-api/releases/download/v#{version}/builtwith-darwin-x64"
-      sha256 "bbab952540d937328c9a47d9cbfcdf8c6b40efe7734fa9f3546d90d266ca29ef"
+      sha256 "819878ebb7404c3ce87e32a94a27935186c0b154b258f156bab79d33369465e5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/zcaceres/builtwith-api/releases/download/v#{version}/builtwith-linux-arm64"
-      sha256 "ee561c117c531821e6d5afe1a3c0f3877cfe5130a21da47aac8a7ce6bdd23f18"
+      sha256 "f3952ee49e817826348a1e1a364d86e3d3366d4879ee9bac90cebb245f52ecbb"
     else
       url "https://github.com/zcaceres/builtwith-api/releases/download/v#{version}/builtwith-linux-x64"
-      sha256 "a6e993323f093b31e5ee893c882edac53b9857e9c15e97df529c56f741330186"
+      sha256 "dc480b214dc8ceabd1ae026d7c1a3b4eba3c61c8db7a17b5fcc9602af525c583"
     end
   end
 
